cd20ee271b27b04c2cf4933c322051bd3a50769c
DESKTOP-G0TT3JA\Usuário <joao.jbosco@gmail.com>
2024-01-05 17:01:02 -0300

    Usando o tipo byte, nesse exemplo.

--- ConsoleAppTypes/Program.cs
+++ ConsoleAppTypes/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 sbyte Byte = 100;
+byte b = 126;
 // Or SByte Byte = 100 =  namespaces System
 Console.Write(Byte);
+Console.WriteLine();
+Console.Write(b);
